b053937813d2cf8f3c9de62fe956524ca505df00
nathansire <nathantech2005@gmail.com>
2018-06-07 16:36:54 -0400

    more friendly user input

--- check.py
+++ check.py
@@ -5,7 +5,10 @@ import sys
 def getmd5(filename):
     return hashlib.md5(open(filename, 'rb').read()).hexdigest()
 
-if sys.argv[2] == getmd5(sys.argv[1]):
+file = input("Enter file name:")
+md5 = input("Enter md5 checksum to compare:")
+
+if md5 == getmd5(file):
     print("MD5 Checksum passed.")
 else:
     print("MD5 Checksum failed. Incorrect MD5.")
